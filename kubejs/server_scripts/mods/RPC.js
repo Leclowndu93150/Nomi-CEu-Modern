c57b1c184ca1a1d432acec1b44cac4b9d4c84679
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-04-01 13:14:30 +0200

    I HATE THIS MOD

--- kubejs/server_scripts/mods/RPC.js
+++ kubejs/server_scripts/mods/RPC.js
@@ -1,9 +1,21 @@
-FTBQuestsEvents.completed('319FB8E210480B5A', event => {
-	SDRP.setState(`Just started nomifactory!`, event.player.username, 'creative')
+PlayerEvents.loggedIn(event => {
+    if (FTBQuests.getServerDataFromPlayer(event.player.UUID).isCompleted('319FB8E210480B5A')) {
+        event.player.stages.add('beforelv')
+    }
+
+
+    if (event.player.stages.has('beforelv')) {
+        global.current_tier = "beforelv"
+    }
+
 })
 
 PlayerEvents.loggedIn(event => {
-	const name = capitalize(event.level.dimension.path);
-	// i have no clue why tf is this working on the server
-	SDRP.setState(`In ${name}`, name, "creative")
+    if (global.current_tier === "beforelv") {
+        global.name = "Stage: Before LV"
+    } else {
+        global.name = "Stage: " + current_tier
+    }
+
+    SDRP.setState(`Gregifying their World`, global.name, `${global.current_tier}`)
 })
